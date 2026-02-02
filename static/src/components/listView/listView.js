92fe574e68c903f6ccee9c005b6272c0bfa562f4
Jamila EL ABBASI <jamilaelabbasi@gmail.com>
2026-02-02 22:05:32 +0000

    component lifecycle.setup() & useState()

--- static/src/components/listView/listView.js
+++ static/src/components/listView/listView.js
@@ -1,10 +1,27 @@
 /* @odoo-module */
 
-import { Component } from "@odoo/owl";
+import { Component , useState} from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
+//# pour utiliser ORM methode je dois utiliser hooks pour recuperer les donnees depui DB
 
 export class ListViewAction extends Component{
     static template="app_one.ListView"
+
+
+    setup(){
+       this.state=useState({'records':[]});
+       this.orm=useService("orm");
+       this.loadRecords();
+    };
+
+
+    async loadRecords(){
+    const result= await this.orm.searchRead("property",[],["name", "postcode"]) // lire les donnees apartir du model property
+    console.log(result)
+    this.state.records=result
+    }
+
 }
 
 registry.category("actions").add("app_one.action_list_view",ListViewAction);
