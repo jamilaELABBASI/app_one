6417ffc4b83dd2517376f910dd757a44eb761cd7
Jamila EL ABBASI <jamilaelabbasi@gmail.com>
2026-02-03 14:59:37 +0000

    using RPC service

--- static/src/components/listView/listView.js
+++ static/src/components/listView/listView.js
@@ -1,33 +1,42 @@
 /* @odoo-module */
 
-import { Component , useState} from "@odoo/owl";
+import { Component, useState, onWillStart } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
-//# pour utiliser ORM methode je dois utiliser hooks pour recuperer les donnees depui DB
 
-export class ListViewAction extends Component{
-    static template="app_one.ListView"
+export class ListViewAction extends Component {
+    static template = "app_one.ListView";
 
+    setup() {
+        this.orm = useService("orm");
 
-    setup(){
-       this.state=useState({'records':[]});
-       this.orm=useService("orm");
-       this.loadRecords();
-    };
+        this.state = useState({
+            records: [],
+        });
 
-
-    async loadRecords(){
-    const result= await this.orm.searchRead("property",[],["name", "postcode"]) // lire les donnees apartir du model property
-    console.log(result)
-    this.state.records=result
+        onWillStart(async () => {
+           const result = await this.orm.searchRead(
+                "property",
+                [],
+                ["id", "name", "postcode", "date_availability"]
+            );
+            this.state.records=result;
+            console.log(result);
+        });
     }
 
+
+//    async loadRecords(){
+//        const result= await this.orm.searchRead("property",[],[]) // lire les donnees apartir du model property
+//        console.log(result)
+//        this.state.records=result
+//        }
 }
 
-registry.category("actions").add("app_one.action_list_view",ListViewAction);
-
-
-
+registry.category("actions").add(
+    "app_one.action_list_view",
+    ListViewAction
+);
 
 
 
